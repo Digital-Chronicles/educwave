2484d191983f9f2c6791eccab7d49238ccc6bb14
ndagirelillian <ndagirelillian15@gmail.com>
2025-03-03 09:54:14 +0300

    finacel module

---
+++ finance/forms.py
@@ -0,0 +1,84 @@
+from django import forms
+from .models import *
+
+class SchoolfeesForm(forms.ModelForm):
+    class Meta:
+        model= SchoolFees
+        fields =[ 'grade','tuitionfee','hostelfee','breakfastfee','lunchfee','description'
+
+        ]
+        widgets={
+            'grade': forms.Select(attrs={'class': 'form-control border-input'}),
+            'tuitionfee': forms.NumberInput(attrs={'class': 'form-control border-input'}),
+            'hostelfee': forms.NumberInput(attrs={'class': 'form-control border-input'}),
+            'breakfastfee': forms.NumberInput(attrs={'class': 'form-control border-input'}),
+            'lunchfee': forms.NumberInput(attrs={'class': 'form-control border-input'}),
+            'description': forms.Textarea(attrs={'class': 'form-control border-input'}),
+
+        }
+
+class OtherSchoolPaymentForm(forms.ModelForm):
+    class Meta:
+        model= OtherSchoolPayments
+        fields =[ 'grade','fees_type','amount','unique_code','description'
+
+        ]
+        widgets={
+            'grade': forms.Select(attrs={'class': 'form-control border-input'}),
+            'fees_type': forms.Select(attrs={'class': 'form-control border-input'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control border-input'}),
+            'unique_code': forms.NumberInput(attrs={'class': 'form-control border-input'}),
+            'description': forms.Textarea(attrs={'class': 'form-control border-input'}),
+
+        }        
+    
+class TransportForm(forms.ModelForm):
+    class Meta:
+        model= TransportFee
+        fields =[ 'location','amount'
+
+        ]
+        widgets={
+            'location': forms.TextInput(attrs={'class': 'form-control border-input'}),
+          
+            'amount': forms.NumberInput(attrs={'class': 'form-control border-input'})
+           
+
+        }       
+
+ 
+class StudentTuitionDescriptionForm(forms.ModelForm):
+    class Meta:
+        model = StudentTuitionDescription
+        fields = ['student', 'tuition', 'hostel', 'lunch', 'breakfast']  
+        widgets = {
+            'student': forms.Select(attrs={'class': 'form-control border-input'}),
+            'tuition': forms.Select(attrs={'class': 'form-control border-input'}),
+            'hostel': forms.CheckboxInput(attrs={'class': 'form-check-input'}),  
+            'lunch': forms.CheckboxInput(attrs={'class': 'form-check-input'}),  
+            'breakfast': forms.CheckboxInput(attrs={'class': 'form-check-input'})   }                 
+
+
+
+class FeeTransactionForm(forms.ModelForm):
+    class Meta:
+        model = FeeTransaction
+        fields = [
+            'student','amount_paid', 'payment_method', 'due_date',
+            'status', 'last_payment_date', 'payment_reference', 'receipt_url', 'remarks'
+        ]
+        widgets = {
+            'student': forms.Select(attrs={'class': 'form-control border-input'}),
+           
+            'amount_paid': forms.NumberInput(attrs={'class': 'form-control border-input', 'step': '0.01'}),
+            'payment_method': forms.Select(attrs={'class': 'form-control border-input'}),
+            'due_date': forms.DateInput(attrs={'class': 'form-control border-input', 'type': 'date'}),
+            'status': forms.Select(attrs={'class': 'form-control border-input'}),
+            'last_payment_date': forms.DateInput(attrs={'class': 'form-control border-input', 'type': 'date'}),
+            'payment_reference': forms.TextInput(attrs={'class': 'form-control border-input'}),
+            'receipt_url': forms.URLInput(attrs={'class': 'form-control border-input'}),
+            'remarks': forms.Textarea(attrs={'class': 'form-control border-input', 'rows': 3}),
+        }
+
+
+
--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,29 @@
 from django.urls import path
-from . import views
+
+from .views import *
 
 urlpatterns = [
-    path("", views.finance, name="finance"),
+    path("", finance, name="finance"),
+#   grade school fees 
+    path('schoolfees/', Schoolfees, name='schoolfees'),
+    path("register/schoolfees/", RegisterSchoolfees.as_view(), name='register_schoolfees'),
+    path('grades/summary/', grade_fee_summary, name='grade_fee_summary'),
+#other school payments
+    path('otherpayment/', OtherSchoolPayment, name='otherpayments'),
+    path("register/otherschool/payments/", RegisterOtherSchoolPayments.as_view(), name='register_otherpayments'),
+#transport
+   
+    path("register/transport/", RegisterTransport.as_view(), name='register_transport'),
+
+#student_tuition_description
+    path('tuition/description/', Student_TuitionDescription, name='tuition_description'),
+    path("register/tuition/description/", RegisterStudentTuitionDescription.as_view(), name='register_tuition_description'),
+
+#feetransaction
+    path('fee/transaction/', Fee_Transaction_list, name='feetransaction'),
+    path('student/<int:id>/transactions/', student_transactions, name='student_transactions'),
+    
+    path("register/fee/transaction", RegisterFeeTransaction.as_view(), name='register_schoolfees'),
+
+
 ]
