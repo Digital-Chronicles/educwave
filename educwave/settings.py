57438607989bbc92e9e7f4b938ffffd8e1caac1c
ndagirelillian <ndagirelillian15@gmail.com>
2025-05-27 10:55:46 +0300

    staic

--- educwave/settings.py
+++ educwave/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Directories where Django will search for additional static files
-STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (uploads by users)
 MEDIA_URL = '/media/'
