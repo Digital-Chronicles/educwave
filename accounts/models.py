f842a611bc2a22c9e0a02b8f4e2a5e90e17014ab
ndagirelillian <ndagirelillian15@gmail.com>
2025-05-28 10:06:15 +0300

    users

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomUser(AbstractUser):
+    name = models.CharField(max_length=150, blank=True)  # Add this line
+
     class Role(models.TextChoices):
         ADMIN = 'ADMIN', _('Admin')
         ACADEMIC = 'ACADEMIC', _('Academic')
