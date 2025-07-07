6b6fce73b315abab812286c26d78ac05c79c309f
ndagirelillian <ndagirelillian15@gmail.com>
2025-07-07 10:28:13 +0300

    right port

--- educwave/settings.py
+++ educwave/settings.py
@@ -100,10 +100,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'school_manager',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'educwave123',
         'HOST':'localhost',
-        'PORT':'3306',
+        'PORT': '5432',
     }
 }
 
