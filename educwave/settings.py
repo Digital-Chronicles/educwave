055634c1dd8642632b63fcc084abe94fb2714fd5
ndagirelillian <https://github.com/ndagirelillian>
2025-11-07 18:57:35 +0300

    postgress

--- educwave/settings.py
+++ educwave/settings.py
@@ -99,23 +99,23 @@ WSGI_APPLICATION = 'educwave.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'educwave',
-#         'USER': 'postgres',
-#         'PASSWORD': 'atwanzire',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educwave',
+        'USER': 'postgres',
+        'PASSWORD': 'atwanzire',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
@@ -161,7 +161,10 @@ USE_TZ = True
 
 #development
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]#development
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')#production
 
 
 
